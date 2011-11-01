b58e6e7167138c226fdceb05c977e0f9e94a973e
Robert May <robotmay@gmail.com>
2011-11-01 12:37:05 +0000

    Switched to hash

--- lib/validates_as_uk_postcode.rb
+++ lib/validates_as_uk_postcode.rb
@@ -1,18 +1,17 @@
 module ValidatesAsUKPostcode
   module Postcodes
-    UK = begin    
-      patterns = [
-        '^[A-PR-UWYZ]{1}\d{1,2}\s?\d[ABD-HJLNP-UWXYZ]{2}$', # AN NAA or ANN NAA
-        '^[A-PR-UWYZ]{1}[A-HK-Y]{1}\d{1,2}\s?\d[ABD-HJLNP-UWXYZ]{2}$', # AAN NAA or AANN NAA
-        '^[A-PR-UWYZ]{1}\d[A-HJKSTUW]{1}\s?\d[ABD-HJLNP-UWXYZ]{2}$', # ANA NAA
-        '^[A-PR-UWYZ]{1}[A-HK-Y]{1}\d[ABEHMNPRVWXY]{1}\s?\d[ABD-HJLNP-UWXYZ]{2}$', # AANA NAA
-        '^(gir)(0aa)$', # GIR 0AA
-        '^(san)(ta1)$', # SAN TA1
-        '^(bfpo)([0-9]{1,4})$', # BFPO (British Forces Post Office)
-        '^(bfpo)(c\/o[0-9]{1,3})$' # BFPO c/o
-      ]
+    UK = begin
+      patterns = {
+        :an_naa_or_ann_naa   => '^[A-PR-UWYZ]{1}\d{1,2}\s?\d[ABD-HJLNP-UWXYZ]{2}$',
+        :aan_naa_or_aann_naa => '^[A-PR-UWYZ]{1}[A-HK-Y]{1}\d{1,2}\s?\d[ABD-HJLNP-UWXYZ]{2}$',
+        :ana_naa             => '^[A-PR-UWYZ]{1}\d[A-HJKSTUW]{1}\s?\d[ABD-HJLNP-UWXYZ]{2}$',
+        :aana_naa            => '^[A-PR-UWYZ]{1}[A-HK-Y]{1}\d[ABEHMNPRVWXY]{1}\s?\d[ABD-HJLNP-UWXYZ]{2}$',
+        :historic_code       => '^(gir)(0aa)$',
+        :bfpo                => '^(bfpo)([0-9]{1,4})$',
+        :bfpo_co             => '^(bfpo)(c\/o[0-9]{1,3})$'
+      }
       
-      pattern = /#{patterns.join "|"}/i
+      pattern = /#{patterns.collect{|k,v| v}.join "|"}/i
     end
   end
 end
